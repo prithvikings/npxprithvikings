02eb81bfb1960aa3a4969f1dfab0447be02b521b
Prithvi Raj <prithvi07raj07@gmail.com>
2026-08-23 20:09:16 +0530

    Initiated the terminal Ui portfolio

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Text, Box } from "ink";
+
+export default function App() {
+  return (
+    <Box flexDirection="column">
+      <Text bold>PRITHVI RAJ</Text>
+      <Text>Full Stack Developer</Text>
+
+      <Text> </Text>
+
+      <Text>Welcome to my terminal portfolio 🚀</Text>
+    </Box>
+  );
+}
