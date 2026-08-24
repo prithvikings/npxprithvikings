d4f95983cc4edda11d16922d34048e751c4e7649
Prithvi Raj <138392587+prithvikings@users.noreply.github.com>
2026-08-24 20:35:27 +0530

    feat: add email contact link

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,4 +1,5 @@
 export const contact = {
   github: "https://github.com/prithvikings",
   linkedin: "https://www.linkedin.com/in/prithvi312/",
+  email: "mailto:prithvi07raj07@gmail.com",
 } as const;
